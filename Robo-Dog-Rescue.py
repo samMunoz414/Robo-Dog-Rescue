a24ed2df5888ae81ea262d778b6f16aba3478c57
reganbragg <59707160+reganbragg@users.noreply.github.com>
2020-01-16 10:37:01 -0500

    Add files via upload

--- Robo-Dog-Rescue.py
+++ Robo-Dog-Rescue.py
@@ -8,6 +8,7 @@ import os
 from Levels import *
 from Characters import *
 from Blocks import *
+from enum import Enum
 
 # Initialize pygame
 pygame.init()
@@ -18,85 +19,112 @@ try:
 except:
 	print ("Fonts Unavailable")
 	sys.exit()
+
+# Game loop for start screen
+def start():
+	print('On start screen')
+	background = pygame.image.load('startScreen.png').convert_alpha()
+	backgroundbox = background.get_rect()
+	while 1:
+		screen.blit(background, backgroundbox)
+		clock.tick(30)
+		pygame.display.flip()
+
+# Game loop for level one
+def levelone():
+    print('In level one function')
+    background = pygame.image.load("background.png").convert_alpha()
+    backgroundbox = background.get_rect()
+    
+    # create a level object
+    level = Level()
+        
+    # Make a list of enemies
+    platform_list = Level.platform(1)
+    platform_list.add(Level.floor(1))
+    platform_list.add(Level.powerups(1))
+    enemy_list = Level.enemy(1, 500, 570)
+    platform_list.add(enemy_list)
+    
+    # Spawn person and add input booleans
+    grace = Person('tall_blue.png', 60, 570)
+    person_list = pygame.sprite.Group()
+    person_list.add(grace)
+    
+    # booleans for input
+    up = down = left = right = False
+
+    while 1:
+        
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT or event.key == ord('a'):
+                    print('left')
+                    left = True
+                if event.key == pygame.K_RIGHT or event.key == ord('d'):
+                    print('right')
+                    right = True
+                if event.key == pygame.K_UP or event.key == ord('w'):
+                    print('up')
+                    up = True
+        
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_LEFT or event.key == ord('a'):
+                    print('left stop')
+                    left = False
+                if event.key == pygame.K_RIGHT or event.key == ord('d'):
+                    print('right stop')
+                    right = False
+                if event.key == pygame.K_UP or event.key == ord('w'):
+                    print('up stop')
+                    up = False
+                if event.key == ord('q'):
+                    print("Exiting Robo-Dog Rescue")
+                    pygame.quit()
+                    sys.exit()
+                
+            if event.type == pygame.QUIT:
+                sys.exit()
+                        
+        screen.blit(background, backgroundbox)
+        grace.update(up, down, left, right, level, platform_list)
+        person_list.draw(screen)
+        platform_list.draw(screen)
+        # powerups_list.draw(screen)
+        for enemy in enemy_list:
+            enemy.move()
+        clock.tick(30)
+        pygame.display.flip()
         
 #################### Create Content #######################
+
+# Fields needed for running program
+running = True
+state = 'START'
 
 # Create a screen (width, height)
 screenx = 960
 screeny = 720
-# ty = 100
 screen = pygame.display.set_mode((screenx, screeny))
-background = pygame.image.load("background.png").convert_alpha()
-backgroundbox = background.get_rect()
 pygame.display.set_caption('Robo-Dog Rescue')
-
-# create a level object
-level = Level()
-
-# Make a list of enemies
-platform_list = Level.platform(1)
-enemy_list = Level.enemy(1, 500, 570)
-platform_list.add(enemy_list)
-platform_list.add(Level.floor(1))
-platform_list.add(Level.powerups(1))
-
-# Spawn person and add input booleans
-grace = Person('tall_blue.png', 60, 570)
-person_list = pygame.sprite.Group()
-person_list.add(grace)
-steps = 10
-# booleans for input
-up = down = left = right = False
 
 # Clock
 clock = pygame.time.Clock()
 
-
-####################### Main Event Loop #########################
-# go into a holding pattern until someone clicks a mouse or hits a key
-
-while 1:
-	# runs death sequence if the player dies
-	if not grace.isAlive:
-		print("You lose")
-		sys.exit()
-
-	for event in pygame.event.get():
-		if event.type == pygame.KEYDOWN:
-			if event.key == pygame.K_LEFT or event.key == ord('a'):
-			    print('left')
-			    left = True
-			if event.key == pygame.K_RIGHT or event.key == ord('d'):
-			    print('right')
-			    right = True
-			if event.key == pygame.K_UP or event.key == ord('w'):
-			    print('up')
-			    up = True
-		    
-		if event.type == pygame.KEYUP:
-			if event.key == pygame.K_LEFT or event.key == ord('a'):
-			    print('left stop')
-			    left = False
-			if event.key == pygame.K_RIGHT or event.key == ord('d'):
-			    print('right stop')
-			    right = False
-			if event.key == pygame.K_UP or event.key == ord('w'):
-			    print('up stop')
-			    up = False
-			if event.key == ord('q'):
-			    print("Exiting Robo-Dog Rescue")
-			    pygame.quit()
-			    sys.exit()
-
-		if event.type == pygame.QUIT:
-			sys.exit()
-    
-	screen.blit(background, backgroundbox)
-	grace.update(up, down, left, right, level, platform_list)
-	person_list.draw(screen)
-	platform_list.draw(screen)
-	for enemy in enemy_list:
-		enemy.move()
-	clock.tick(30)
-	pygame.display.flip()
-            
+while running:
+    if state == 'LEVELONE':
+        print('In level one state')
+        levelone()
+    if state == 'START':
+        print('In the start state')
+        start()
+    if state == 'END':
+        pass
+    if state == 'TUTORIAL':
+        pass
+    if state == 'PROLOGUE':
+        pass
+    if state == 'WIN':
+        pass
+    if state == 'LOSE':
+        pass
