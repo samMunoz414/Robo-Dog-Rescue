96d35466904a9d23382286e5cb1f311e418ede90
Samuel Munoz <smunoz23@imac-025289.colby.edu>
2020-01-20 09:17:25 -0500

    testing merges

--- Buttons.py
+++ Buttons.py
@@ -21,3 +21,7 @@ class Button:
 	# checks if the mouses position interlaps with button's position
 	def isClicked(self, mousePosition):
 		return self.rect.collidepoint(mousePosition)
+
+    # testing merging method one
+    def event(self):
+        pass
